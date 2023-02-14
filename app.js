6dc48b002c3319610e906bf1feabe1e098b0e6ad
Zergo12 <adzg96@gmail.com>
2023-02-13 21:30:11 -0600

    Ya se pueden crear eventos en calendario

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const hbs = require("hbs");
 
 const app = express();
 
+
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
 require("./config")(app);
 
@@ -32,6 +33,8 @@ app.use("/auth", authRoutes);
 
 const userRoutes = require("./routes/user.routes");
 app.use("/user", userRoutes);
+
+
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,5 +1,22 @@
 const express = require("express");
 const router = express.Router();
+const axios = require('axios');
+
+
+// API Google
+const { google } = require("googleapis")
+
+const { OAuth2 } = google.auth
+
+const googleApi = new OAuth2(
+    process.env.CLIENT_ID,
+    process.env.CLIENT_SECRET,
+    process.env.REDIRECT_URL,
+    process.env.API_KEY,
+);
+
+
+const scope = ['https://www.googleapis.com/auth/calendar']
 
 // Require the User model in order to interact with the database
 const User = require("../models/User.model");
@@ -7,15 +24,35 @@ const User = require("../models/User.model");
 // Require necessary (isLoggedOut and isLiggedIn) middleware in order to control access to specific routes
 const isLoggedOut = require("../middleware/isLoggedOut");
 const isLoggedIn = require("../middleware/isLoggedIn");
+const { compare } = require("bcrypt");
 
 
 router.get("/profile", isLoggedOut, (req, res) => {
     res.render("user/profile");
   });
 
-
 router.get("/spa", isLoggedOut, (req, res) => {
-  res.render("user/spa");
+  res.render("user/appointment");
+});
+
+
+router.get("/google", async (req,res) => {
+  const {code} = req.query
+  console.log({code})
+
+  if (code) {
+    const { tokens } = await googleApi.getToken(code);
+    googleApi.setCredentials(tokens);
+    console.log(tokens)
+    res.redirect('/user/profile')
+  } else {
+    const url = googleApi.generateAuthUrl({
+      access_type: "offline",
+      scope: scope
+    });
+    res.redirect(url);
+    console.log ("--", url) 
+  }
 });
 
   module.exports = router;
