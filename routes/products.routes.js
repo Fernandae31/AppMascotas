af056944850fc418a43c1c1011af3aefa780c04d
Zergo12 <adzg96@gmail.com>
2023-02-15 10:37:29 -0600

    Se agrego rol de admin

--- routes/products.routes.js
+++ routes/products.routes.js
@@ -2,15 +2,36 @@ const { Router } = require("express")
 const router = Router()
 const Product = require("../models/Product.model")
 
+function isAdmin (currentUser) {
+    if(currentUser.role === "admin") {
+        return true
+    } else {
+        return false
+    }
+}
+
 router.get("/cats", (req, res) => {
     Product
-    .find()
+    .find({target:"Cat"})
     .then((Product) => {
         console.log(Product)
-        res.render("products/cats", {product: Product})
+        console.log (isAdmin(req.session.currentUser)) 
+        res.render("products/cats", {product: Product, admin:isAdmin(req.session.currentUser)})
     })
     .catch(err => next(err))
 })
+
+router.get("/dogs", (req, res, next) => {
+    Product
+    .find({target:"Dog"})
+    .then((Product) => {
+        console.log(Product)
+        res.render("products/dogs", {product: Product})
+    })
+    .catch(err => next(err))
+})
+
+
 
 router.get("/dogs", (req, res) => {
     res.render("products/dogs")
@@ -25,10 +46,16 @@ router.post('/newProduct', (req,res, next)  => {
     Product
     .create({name, price, description, category, quantity, photo, target})
     .then((product) => {
-        res.redirect("/products/cats")
+        res.redirect("/")
     })
     .catch((err) => next(err))
 })
+
+// router.get("/editProduct/:id", (req,res) => {
+//     const { id } = req.params
+//     const { }
+//     res.render('products/editProduct')
+// })
 
 
 module.exports = router
